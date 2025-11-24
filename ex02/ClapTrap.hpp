86f6df44bce98bd35090e30172bd9c16334094d8
hennahp <hparveen777@gmail.com>
2025-11-24 13:35:21 +0400

    started ex02

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 12:20:04 by hparveen          #+#    #+#             */
+/*   Updated: 2025/11/24 10:00:18 by hparveen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define RESET "\033[0m"
+
+class ClapTrap
+{
+  protected:
+	std::string		_name;
+	int				_hitPoints;
+	int				_energyPoints;
+	int 			_attackDamage;
+
+  public:
+	ClapTrap();
+	ClapTrap(const std::string &name);
+	ClapTrap(const ClapTrap &other);
+	ClapTrap &operator=(const ClapTrap &other);
+	~ClapTrap();
+
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 08:57:34 by hparveen          #+#    #+#             */
+/*   Updated: 2025/11/24 13:33:33 by hparveen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+int	main(void)
+{
+	std::cout << "\n--- Creating ScavTrap ---\n" << std::endl;
+    ScavTrap scav("Serena");
+    
+    std::cout << "\n--- Testing Attack ---\n" << std::endl;
+    scav.attack("Target Bot");
+
+    std::cout << "\n--- Testing special ability ---\n" << std::endl;
+    scav.guardGate();
+
+    std::cout << "\n--- Testing Copy Constructor ---\n" << std::endl;
+    ScavTrap scavClone(scav);
+    scavClone.attack("Clone Target Bot");
+
+    std::cout << "\n--- Testing Copy Assignment Constructor ---\n" << std::endl;
+    ScavTrap scavCopy;
+    scavCopy = scav;
+
+    std::cout << "\n--- Testing Damage and Repair ---\n" << std::endl;
+    scav.takeDamage(30);
+    scav.beRepaired(20);
+    
+    std::cout << "\n--- Destroying ScavTrap ---\n" << std::endl;
+    return 0;
+}
